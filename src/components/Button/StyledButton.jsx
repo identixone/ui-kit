756701af6a20c231080ce6523eb8ebf7fa9c76cb
Dmitriy Balashov <mockedcat@gmail.com>
2019-09-06 11:03:53 +0300

    Button now always has display block prop

--- src/components/Button/StyledButton.jsx
+++ src/components/Button/StyledButton.jsx
@@ -5,6 +5,7 @@ import ButtonSizes from "./ButtonSizes";
 import ButtonFits from "./ButtonFits";
 
 const StyledButton = styled.button`
+  display: block;
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   outline: ${({ isHardOutline }) =>
     isHardOutline ? "none !important" : "none"};
