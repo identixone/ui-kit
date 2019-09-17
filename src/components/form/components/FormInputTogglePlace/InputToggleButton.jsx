cf27ce080b455aa9d89365350023b20160fff58f
Nikita Kashtanov <nka@datacorp.ee>
2019-09-17 10:53:50 +0300

    form input with input permanent

---
+++ src/components/form/components/FormInputTogglePlace/InputToggleButton.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+import Button from "../../../Button";
+
+import { FormUneditableFieldStyles } from "../FormUneditableField/StyledFormUneditableField";
+
+const InputToggleButton = styled(Button).attrs({ buttonTheme: "reset" })`
+  ${FormUneditableFieldStyles}
+
+  text-decoration: underline dotted;
+  border-bottom: 1px dotted
+    ${({ hasValue }) => (!hasValue ? "#000" : "transparent")};
+`;
+
+export default InputToggleButton;
