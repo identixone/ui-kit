c45937f237f6b33b43d97b8c407af5d688e20639
Nikita Kashtanov <nka@datacorp.ee>
2019-09-18 13:38:35 +0300

    multiselet height fix

--- src/components/form/components/FormMultiSelect/FormMultiSelectInputWrapper.jsx
+++ src/components/form/components/FormMultiSelect/FormMultiSelectInputWrapper.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const FormMultiSelectInput = styled.li`
   margin-bottom: 8px;
-  width: 171px;
+  width: 332px;
 `;
 
 export default FormMultiSelectInput;
