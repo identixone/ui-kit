64dc7491f55ee9758f9bff833dabc7259883602f
Nikita Kashtanov <nka@datacorp.ee>
2019-09-19 17:31:18 +0300

    multiselect layout changes

--- src/components/form/components/FormMultiSelect/FormMultiSelectInput.jsx
+++ src/components/form/components/FormMultiSelect/FormMultiSelectInput.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const FormMultiSelectInput = styled.input`
   background-color: transparent;
+  width: 350px;
   border: none;
   outline: none;
   display: inline-block;
