f3587a3bc9a42295cb35f0a650fe6472e98b353a
Dmitriy Balashov <mockedcat@gmail.com>
2019-10-02 17:20:07 +0300

    PopupConfirm component was added

---
+++ src/components/PopupConfirm/PopupConfirmPopup/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { StyledPopupConfirmPopup } from "./StyledPopupConfirmPopup";
+import { PopupConfirmPopupTitle } from "./PopupConfirmPopupTitle";
+import { PopupConfirmPopupButton } from "./PopupConfirmPopupButton";
+import { PopupConfirmPopupButtons } from "./PopupConfirmPopupButtons";
+
+function PopupConfirmPopup({ title, okText, cancelText, onConfirm, onCancel }) {
+  return (
+    <StyledPopupConfirmPopup>
+      <PopupConfirmPopupTitle>{title}</PopupConfirmPopupTitle>
+      <PopupConfirmPopupButtons>
+        <PopupConfirmPopupButton
+          buttonTheme="dark"
+          size="large"
+          onClick={onConfirm}
+        >
+          {okText}
+        </PopupConfirmPopupButton>
+        <PopupConfirmPopupButton
+          buttonTheme="outline"
+          size="large"
+          onClick={onCancel}
+        >
+          {cancelText}
+        </PopupConfirmPopupButton>
+      </PopupConfirmPopupButtons>
+    </StyledPopupConfirmPopup>
+  );
+}
+
+PopupConfirmPopup.propTypes = {
+  title: PropTypes.string,
+  okText: PropTypes.string,
+  cancelText: PropTypes.string,
+  onConfirm: PropTypes.string.isRequired,
+  onCancel: PropTypes.string.isRequired,
+};
+
+PopupConfirmPopup.defaultProps = {
+  title: "Are you sure?",
+  okText: "Ok",
+  cancelText: "Cancel",
+};
+
+export { PopupConfirmPopup };
---
+++ src/components/PopupConfirm/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import usePortal from "react-useportal";
+
+import { PopupConfirmPopup } from "./PopupConfirmPopup";
+
+function PopupConfirm({
+  children,
+  title,
+  okText,
+  cancelText,
+  onConfirm,
+  portalRef,
+}) {
+  const [openPopup, closePopup, isPopupOpen, Portal] = usePortal({
+    onOpen({ portal, targetEl }) {
+      const { top, left } = targetEl.getBoundingClientRect();
+
+      /**
+       * Так сделано, потому что нужно отрисовать
+       * элемент в конкретном месте над триггером
+       */
+      portal.current.style.cssText = `
+          position: absolute;
+          top: ${top - 95 / 2}px;
+          left: ${left - 239 / 2 + 65}px;
+        `;
+    },
+  });
+
+  function togglePopup(...args) {
+    if (isPopupOpen) {
+      closePopup(...args);
+    } else {
+      openPopup(...args);
+    }
+  }
+
+  return (
+    <React.Fragment>
+      {children({ openPopup, closePopup, togglePopup, isPopupOpen })}
+      {isPopupOpen && (
+        <Portal ref={portalRef}>
+          <PopupConfirmPopup
+            title={title}
+            okText={okText}
+            cancelText={cancelText}
+            onConfirm={onConfirm}
+            onCancel={closePopup}
+          />
+        </Portal>
+      )}
+    </React.Fragment>
+  );
+}
+
+PopupConfirm.propTypes = {
+  children: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  okText: PropTypes.string,
+  cancelText: PropTypes.string,
+  onConfirm: PropTypes.string.isRequired,
+  portalRef: PropTypes.object,
+};
+
+export { PopupConfirm };
