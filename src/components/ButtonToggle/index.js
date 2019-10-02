83461287f814342f9b58b781163db60aa50f66e6
Nikita Kashtanov <nka@datacorp.ee>
2019-10-02 18:45:13 +0300

    export

--- src/components/ButtonToggle/index.js
+++ src/components/ButtonToggle/index.js
@@ -11,7 +11,7 @@ import { ButtonToggleWrapper } from "../ButtonToggleWrapper";
 
 import { Times } from "../../assets/icons";
 
-function ButtonToggle({ children, icon, mode, onChange }) {
+export function ButtonToggle({ children, icon, mode, onChange }) {
   return (
     <ButtonToggleWrapper
       onChange={onChange}
