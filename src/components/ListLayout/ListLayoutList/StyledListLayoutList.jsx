03f698a15737b6351f7002b0c05b2a0ebf079a47
Dmitriy Balashov <mockedcat@gmail.com>
2019-10-08 14:51:25 +0300

    ListLayoutList now has grid layout cuz columns

--- src/components/ListLayout/ListLayoutList/StyledListLayoutList.jsx
+++ src/components/ListLayout/ListLayoutList/StyledListLayoutList.jsx
@@ -1,22 +1,23 @@
 import styled from "styled-components";
 
-import { StyledListLayoutListItem } from "./ListLayoutListItem";
-
 export const StyledListLayoutList = styled.ul`
   padding-left: 0;
   margin: 0;
   list-style-type: none;
   min-height: 350px;
-  column-count: ${({ columns }) => columns};
-  column-gap: 20px;
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    ${({ columns }) => `calc(${100 / columns}% - ${10 * (columns - 1)}px)`}
+  );
+  grid-auto-rows: max-content;
+  grid-column-gap: 20px;
 
   &:not(:only-child) {
     margin-bottom: 27px;
   }
 
   &:not(:first-child) {
-    ${StyledListLayoutListItem} {
-      border-top: 1px solid #dadada;
-    }
+    border-top: 1px solid #dadada;
   }
 `;
