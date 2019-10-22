d3c1a152848412e195499ac65b12255c7a614755
Dmitriy Balashov <mockedcat@gmail.com>
2019-10-22 15:38:34 +0300

    Value now has `valueToCheck` prop

--- src/components/Value.jsx
+++ src/components/Value.jsx
@@ -2,14 +2,17 @@ import PropTypes from "prop-types";
 
 import { isNotEmpty } from "../utils/helpers";
 
-export function Value({ children, defaultValue, isZeroEmpty }) {
-  return isNotEmpty(children, isZeroEmpty) ? children : defaultValue;
+export function Value({ children, defaultValue, isZeroEmpty, value }) {
+  const valueToCheck = value || children;
+
+  return isNotEmpty(valueToCheck, isZeroEmpty) ? children : defaultValue;
 }
 
 Value.propTypes = {
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.node]),
   defaultValue: PropTypes.any,
   isZeroEmpty: PropTypes.bool,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 Value.defaultProps = {
