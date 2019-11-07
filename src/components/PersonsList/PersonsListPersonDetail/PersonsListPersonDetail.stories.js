bba2a6c75c547978f4af545354da445c95e82ca9
Dmitriy Balashov <mockedcat@gmail.com>
2019-11-07 16:21:55 +0300

    PersonListPersonDetail: component now uses PersonsCardDetailed

--- src/components/PersonsList/PersonsListPersonDetail/PersonsListPersonDetail.stories.js
+++ src/components/PersonsList/PersonsListPersonDetail/PersonsListPersonDetail.stories.js
@@ -2,24 +2,21 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { object, boolean } from "@storybook/addon-knobs";
 
-import { PersonsListPersonDetail } from "./index.jsx";
+import { PersonsListPersonDetail } from "./index";
 
 import { personMock } from "../../../../test/__mocks__";
 
-storiesOf("Persons Lists|PersonsListPersonDetail", module).add(
-  "default",
-  () => {
-    const person = object("Person", personMock);
+storiesOf("Persons Lists| PersonDetail", module).add("default", () => {
+  const person = object("Person", personMock);
 
-    const isLoading = boolean("isLoading", false);
-    const isPersonNotExists = boolean("isPersonNotExists", false);
+  const isLoading = boolean("isLoading", false);
+  const isPersonNotExists = boolean("isPersonNotExists", false);
 
-    return (
-      <PersonsListPersonDetail
-        person={!isPersonNotExists && person}
-        isLoading={isLoading}
-        isPersonNotExists={isPersonNotExists}
-      />
-    );
-  }
-);
+  return (
+    <PersonsListPersonDetail
+      person={!isPersonNotExists && person}
+      isLoading={isLoading}
+      isPersonNotExists={isPersonNotExists}
+    />
+  );
+});
