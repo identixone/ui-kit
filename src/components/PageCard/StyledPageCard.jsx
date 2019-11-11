f7472b6c428461d1cf05f08e8359c1ed33c2967b
Dmitriy Balashov <mockedcat@gmail.com>
2019-11-11 16:36:47 +0300

    PageCard: hardcoded min height was removed

--- src/components/PageCard/StyledPageCard.jsx
+++ src/components/PageCard/StyledPageCard.jsx
@@ -6,7 +6,6 @@ const PageCard = styled.section`
   padding: 35px 75px;
   border-radius: 5px;
   box-shadow: 0px 0px 80px 0px rgba(162, 182, 189, 0.2);
-  min-height: 400px;
 `;
 
 export default PageCard;
