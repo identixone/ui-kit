b0126c6b46182a1892e4bf3122247290da10853d
Nikita Kashtanov <nka@datacorp.ee>
2019-11-19 10:55:17 +0300

    prop type

--- src/components/Search/SearchClearButton/index.jsx
+++ src/components/Search/SearchClearButton/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { StyledSearchClearButton } from "./StyledSearchClearButton";
 import { SearchClearButtonIcon } from "./SearchClearButtonIcon";
@@ -6,9 +7,17 @@ import { SearchClearButtonIcon } from "./SearchClearButtonIcon";
 function SearchClearButton(props) {
   return (
     <StyledSearchClearButton {...props} data-testid="search-clear-btn">
-      <SearchClearButtonIcon size="23" />
+      <SearchClearButtonIcon size={props.size} />
     </StyledSearchClearButton>
   );
 }
+
+SearchClearButton.propTypes = {
+  size: PropTypes.number,
+};
+
+SearchClearButton.defaultProps = {
+  size: 23,
+};
 
 export { SearchClearButton, StyledSearchClearButton };
