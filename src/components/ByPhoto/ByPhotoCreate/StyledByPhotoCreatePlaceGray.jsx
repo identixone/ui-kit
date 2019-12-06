7b30f422ed26253918ee1d4e26aa4b5739e4c987
Dmitriy Balashov <mockedcat@gmail.com>
2019-12-06 10:53:35 +0300

    FormDropdown: API update (children --> renderItem) (#71)
    * FormDropdown: now supports `renderItem` instead of children
    
    * FormDropdown: rewrite to hooks
    
    * InfiniteDropdown: base implementation was added
    
    * FormDropdown: now sets highlighted to 0 if selected undefined
    
    * ByPhoto: components removed
    
    * InfiniteDropdown: stories added
    
    * FormDropdown: fixed default highlight index calc

--- src/components/ByPhoto/ByPhotoCreate/StyledByPhotoCreatePlaceGray.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import StyledPlaceRound from "../StyledPlaceRound";
-
-const StyledByPhotoCreatePlaceGray = styled(StyledPlaceRound)`
-  color: #000;
-  background-color: #efefef;
-`;
-
-export default StyledByPhotoCreatePlaceGray;
---
+++ src/components/InifiniteDropdown/index.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { useState, useEffect } from "react";
+import { useListFetch } from "../../hooks";
+
+import { InfiniteScroll, FormDropdown } from "../index";
+
+function InifiniteDropdown({
+  value,
+  onChange,
+  options,
+  fetchOptions,
+  isFetching,
+  totalCount,
+  limit,
+  placeholder,
+  "data-testid": testId,
+  name,
+  className,
+  renderItem,
+  withSearch,
+  width,
+}) {
+  const [isListEnds, setIsListEnds] = useState(false);
+
+  const { pagination, setPagination, setFetchParams } = useListFetch({
+    fetchList: params => {
+      fetchOptions({ ...params, meta: { clearList: params.offset === 0 } });
+    },
+    pagination: { limit, offset: 0 },
+  });
+
+  useEffect(() => {
+    setIsListEnds(pagination.offset + pagination.limit > totalCount);
+  }, [options]);
+
+  function handleSearch(value) {
+    setFetchParams({
+      q: value,
+      offset: 0,
+    });
+  }
+
+  return (
+    <InfiniteScroll
+      onScrollToPoint={() => {
+        if (!isListEnds) {
+          setPagination({ offset: pagination.offset + pagination.limit });
+        }
+      }}
+      isFetching={isFetching}
+    >
+      {({ scrollerRef }) => (
+        <FormDropdown
+          className={className}
+          withSearch={withSearch}
+          value={value}
+          options={options}
+          onChange={onChange}
+          data-testid={testId}
+          name={name}
+          onInputChange={handleSearch}
+          listRef={scrollerRef}
+          placeholder={placeholder}
+          renderItem={renderItem}
+          width={width}
+        />
+      )}
+    </InfiniteScroll>
+  );
+}
+
+InifiniteDropdown.Option = FormDropdown.Option;
+
+InifiniteDropdown.propTypes = {
+  value: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  fetchOptions: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  placeholder: PropTypes.string,
+  totalCount: PropTypes.number.isRequired,
+  limit: PropTypes.number.isRequired,
+  "data-testid": PropTypes.string,
+  name: PropTypes.string,
+  className: PropTypes.string,
+  renderItem: PropTypes.func,
+  withSearch: PropTypes.bool,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+};
+
+InifiniteDropdown.defaultProps = {
+  limit: 10,
+  "data-testid": "infinite-dropdown",
+};
+
+export { InifiniteDropdown };
