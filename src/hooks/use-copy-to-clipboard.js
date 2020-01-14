89b5484da19d381432018c98c666817397475952
Dmitriy Balashov <mockedcat@gmail.com>
2020-01-14 13:24:39 +0300

    issue with FF useCopyToClipboard was fixed

--- src/hooks/use-copy-to-clipboard.js
+++ src/hooks/use-copy-to-clipboard.js
@@ -9,20 +9,21 @@ function useCopyToClipboard() {
   const copyToClipboard = text => {
     setIsCopied(false);
 
+    if (typeof text == "string" || typeof text == "number") {
+      const element = document.createElement("textarea"); // create textarea HTML element
+      element.value = text; // add the text to be copied to the element
+      document.body.appendChild(element); // add element to DOM
+      element.select(); // select the text
+      document.execCommand("copy"); // execute copy command
+      document.body.removeChild(element); // remove element from DOM
+    } else {
+      console.error(
+        `Cannot copy typeof ${typeof text} to clipboard, must be a valid string or number.`
+      );
+    }
+
     setTimeout(() => {
-      if (typeof text == "string" || typeof text == "number") {
-        const element = document.createElement("textarea"); // create textarea HTML element
-        element.value = text; // add the text to be copied to the element
-        document.body.appendChild(element); // add element to DOM
-        element.select(); // select the text
-        document.execCommand("copy"); // execute copy command
-        document.body.removeChild(element); // remove element from DOM
-        setIsCopied(true);
-      } else {
-        console.error(
-          `Cannot copy typeof ${typeof text} to clipboard, must be a valid string or number.`
-        );
-      }
+      setIsCopied(true);
     });
   };
 
