e93904c0af7d0746b546b0fba39c43259f1aeb89
Nikita Kashtanov <nka@datacorp.ee>
2020-01-15 15:08:43 +0300

    remove language detector usage from kit

--- src/components/I18nProvider.js
+++ src/components/I18nProvider.js
@@ -3,12 +3,11 @@ import PropTypes from "prop-types";
 
 import i18n from "i18next";
 import { I18nextProvider } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 import locales from "../locales";
 
 function I18nProvider({ children }) {
-  i18n.use(LanguageDetector).init({
+  i18n.init({
     resources: locales,
     lng: localStorage.getItem("lng"),
     fallbackLng: "en",
