e8896854477f693469d34326afcea21f9a5f34fc
Dmitriy Balashov <mockedcat@gmail.com>
2020-01-16 16:21:35 +0300

    FormMultiSelectTagsItemTitle: long names now slice with ellipsis

--- src/components/form/components/FormMultiSelect/FormMultiSelectTags/FormMultiSelectTagsItem/FormMultiSelectTagsItemTitle.jsx
+++ src/components/form/components/FormMultiSelect/FormMultiSelectTags/FormMultiSelectTagsItem/FormMultiSelectTagsItemTitle.jsx
@@ -3,6 +3,11 @@ import styled from "styled-components";
 const FormMultiSelectTagsItemTitle = styled.span`
   margin-right: 4px;
   vertical-align: middle;
+  display: inline-block;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 export default FormMultiSelectTagsItemTitle;
