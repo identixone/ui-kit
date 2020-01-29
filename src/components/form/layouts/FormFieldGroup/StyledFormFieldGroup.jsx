f8307b04de1739ad9b1bde5afcf0c2674ca83820
Dmitriy Balashov <mockedcat@gmail.com>
2020-01-29 15:33:38 +0300

    Form Layouts: exports refactoring

--- src/components/form/layouts/FormFieldGroup/StyledFormFieldGroup.jsx
+++ src/components/form/layouts/FormFieldGroup/StyledFormFieldGroup.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
-const StyledFormFieldGroup = styled.div``;
+const { StyledFormFieldGroup } = styled.div``;
 
-export default StyledFormFieldGroup;
+export { StyledFormFieldGroup };
