3ce0a9a7fd64b4d73c610b029056fcaa4426a015
Dmitriy Balashov <mockedcat@gmail.com>
2020-03-31 16:21:16 +0300

    InfiniteMultiSelect: component was added

---
+++ src/components/InfiniteMultiSelect/index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { useInfiniteMenu } from "../../hooks";
+
+import { InfiniteScroll } from "../InfiniteScroll";
+import { FormMultiSelect } from "../components/form";
+
+function InfiniteMultiSelect({
+  name,
+  placeholder,
+  options,
+  fetchOptions,
+  isFetching,
+  limit,
+  hasNext,
+  value,
+  onChange,
+  className,
+  "data-testid": testId,
+}) {
+  const { searchOptions, fetchNext } = useInfiniteMenu({
+    limit,
+    hasNext,
+    isFetching,
+    fetchOptions,
+  });
+
+  return (
+    <InfiniteScroll onScrollToPoint={fetchNext} isFetching={isFetching}>
+      {({ scrollerRef }) => (
+        <FormMultiSelect
+          className={className}
+          value={value}
+          options={options}
+          onChange={onChange}
+          data-testid={testId}
+          name={name}
+          onInputChange={searchOptions}
+          menuRef={scrollerRef}
+          placeholder={placeholder}
+          isLoading={isFetching}
+        />
+      )}
+    </InfiniteScroll>
+  );
+}
+
+InfiniteMultiSelect.Option = FormMultiSelect.Option;
+InfiniteMultiSelect.Menu = FormMultiSelect.Menu;
+
+InfiniteMultiSelect.propTypes = {
+  value: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  fetchOptions: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  placeholder: PropTypes.string,
+  limit: PropTypes.number.isRequired,
+  "data-testid": PropTypes.string,
+  name: PropTypes.string,
+  className: PropTypes.string,
+  hasNext: PropTypes.bool.isRequired,
+};
+
+InfiniteMultiSelect.defaultProps = {
+  limit: 10,
+  "data-testid": "infinite-multi-select",
+};
+
+export { InfiniteMultiSelect };
---
+++ src/hooks/use-infinite-menu.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useListFetch } from "./use-list-fetch";
+
+function useInfiniteMenu({ limit, hasNext, fetchOptions }) {
+  const [isListEnds, setIsListEnds] = useState(hasNext);
+
+  const { pagination, setPagination, setFetchParams } = useListFetch({
+    fetchList: params => {
+      fetchOptions({ ...params, meta: { clearList: params.offset === 0 } });
+    },
+    pagination: { limit, offset: 0 },
+  });
+
+  useEffect(() => {
+    setIsListEnds(hasNext);
+  }, [hasNext]);
+
+  function searchOptions(value) {
+    setFetchParams({
+      q: value,
+      offset: 0,
+    });
+  }
+
+  function fetchNext() {
+    if (!isListEnds) {
+      setPagination({ offset: pagination.offset + pagination.limit });
+    }
+  }
+
+  return {
+    searchOptions,
+    fetchNext,
+  };
+}
+
+export { useInfiniteMenu };
