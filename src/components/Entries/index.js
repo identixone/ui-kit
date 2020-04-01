2dbac17657c1a49443200b749a409aa009ec4fc8
Dmitriy Balashov <mockedcat@gmail.com>
2020-04-01 16:08:43 +0300

    EntriesDateTimeFilter: component export added to entries barrel file

--- src/components/Entries/index.js
+++ src/components/Entries/index.js
@@ -3,5 +3,6 @@ export { EntryPersonCard } from "./EntryPersonCard";
 export { PersonEntriesCard } from "./PersonEntriesCard";
 
 export * from "./components";
+export * from "./EntriesDateTimeFilter";
 
 export * from "./config";
