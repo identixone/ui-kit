e1065a57887eb8759f45f79c42db62116d54bc9f
Dmitriy Balashov <mockedcat@gmail.com>
2020-04-13 17:45:00 +0300

    PageCard: component shadow now visible on siblings

--- src/components/PageCard/StyledPageCard.jsx
+++ src/components/PageCard/StyledPageCard.jsx
@@ -5,6 +5,8 @@ const StyledPageCard = styled.section`
   box-sizing: border-box;
   padding: 32px 64px;
   box-shadow: 0px 0px 80px rgba(162, 182, 189, 0.2);
+  /* чтобы тень была выше окружающих отступов */
+  z-index: 11;
 `;
 
 export { StyledPageCard };
