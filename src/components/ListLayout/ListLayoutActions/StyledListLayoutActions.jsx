4b4d3b1ded88aa56d0d344e7ac996ed6d98330cd
Dmitriy Balashov <mockedcat@gmail.com>
2020-04-16 11:22:54 +0300

    ListLayoutAction: checkbox margin was increased

--- src/components/ListLayout/ListLayoutActions/StyledListLayoutActions.jsx
+++ src/components/ListLayout/ListLayoutActions/StyledListLayoutActions.jsx
@@ -2,13 +2,15 @@ import styled from "styled-components";
 
 import { StyledFormCheckbox } from "../../form/components/FormCheckbox/index";
 
-export const StyledListLayoutActions = styled.div`
+const StyledListLayoutActions = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 18px;
   height: 30px;
 
   ${StyledFormCheckbox}:not(:last-child) {
-    margin-right: 20px;
+    margin-right: 24px;
   }
 `;
+
+export { StyledListLayoutActions };
